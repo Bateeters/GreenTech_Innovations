9fe1c6f94373a03c5ab887a074ad603a469a7d85
Brian Teeters <BrianTeetersDesign@gmail.com>
2024-06-06 11:34:48 -0400

    Added Sticky Nav Bar On Scrolling

--- js/script.js
+++ js/script.js
@@ -61,3 +61,18 @@ showTestimonial();
 setTimeout(() => {
   setInterval(nextTestimonial, 5000);
 }, 5000);
+
+
+
+
+// Sticky nav bar JS
+const stickyNav = document.querySelector('.hide');
+const scrollPoint = 200;
+
+window.addEventListener("scroll", ()=>{
+    if(window.scrollY > scrollPoint) {
+        stickyNav.classList.add("sticky");
+    } else {
+        stickyNav.classList.remove("sticky");
+    }
+});
